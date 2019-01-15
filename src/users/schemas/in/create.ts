7af53754bf8d7e04620e0628d9ae7fdd4b367369
vmakarevich <vmakarevich@exadel.com>
2019-01-15 22:22:19 +0300

    add swagger

--- src/users/schemas/in/create.ts
+++ src/users/schemas/in/create.ts
@@ -1,10 +1,17 @@
 import { IsString } from 'class-validator';
 import { Exclude } from 'class-transformer';
+import {ApiModelProperty} from '@nestjs/swagger'
 
 export class CreateUserDto {
   @IsString()
+  @ApiModelProperty({
+    required: true,
+  })
   readonly name: string;
 
   @IsString()
+  @ApiModelProperty({
+    required: true,
+  })
   readonly password: string;
 }
